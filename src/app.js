4dd3aa4cdbaf92d02d96d07e5204ac94cb72e411
silvak <silvak.jeg@gmail.com>
2022-05-28 05:14:41 -0400

    general design

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import fragmentSphere from "./glsl/sphere.frag";
 import Stats from 'stats-js';
 import "./style.css";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-
 //bloom
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
@@ -12,6 +11,12 @@ import { UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPas
 
 
 const objectSpacing = 6;
+const params = {
+  exposure: 1,
+  bloomStrength: 2.5,
+  bloomThreshold: 0,
+  bloomRadius: 0
+};
 
 //draw fps graph in the DOM
 let stats = new Stats();
@@ -36,17 +41,10 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor( '#18113b' );
+renderer.toneMappingExposure = Math.pow(params.exposure, 4.0);
+//renderer.setClearColor(0x000000);
 
 //-------------------------------------scene-----------------------------------
-// LIGHT
-const light = new THREE.AmbientLight('#18113b');//#fc4c39 #8787ff
-scene.add(light);
-const pointLight = new THREE.PointLight('#fc4c39', 0.8);
-pointLight.position.set(20, 20, 20);
-scene.add(pointLight);
-
-
 //OBJ Sphere 
 const sphereMat = new THREE.ShaderMaterial({
   uniforms: {
@@ -63,11 +61,11 @@ const sphereMat = new THREE.ShaderMaterial({
   wireframe: false
 });
 const sphere = new THREE.Mesh(
-  new THREE.IcosahedronBufferGeometry(1.6, 40), //2,60  o 1.6, 40
+  new THREE.IcosahedronBufferGeometry(1.8, 32), //2,60  o 1.6, 40
   sphereMat
 );
 scene.add(sphere); 
-sphere.position.y = - objectSpacing * 0.06;
+sphere.position.y = - objectSpacing * 0.02;
 sphere.position.x = 1.8;
 
 
@@ -105,10 +103,10 @@ loader.load(
 	function ( obj ) {
     ObjFace = obj;
     ObjFace.position.y= - objectSpacing * 1;
-    ObjFace.position.x = 1.8;
-    ObjFace.position.z = 1;
-    ObjFace.rotation.y = 0.1;
-    ObjFace.scale.set(0.03,0.03,0.03);
+    ObjFace.position.x = -1.6;
+    ObjFace.position.z = 0;
+    ObjFace.rotation.y = 0.50;
+    ObjFace.scale.set(0.02,0.02,0.02);
     ObjFace.traverse(function(child) {
       if (child instanceof THREE.MeshStandardMaterial) {
           child.wireframe = true
@@ -130,14 +128,14 @@ loader.load(
 
 
 
-//---------------------------------------------------------------------------------
-const params = {
-  exposure: 1,
-  bloomStrength: 0,
-  bloomThreshold: 5,
-  bloomRadius: 1
-};
+// LIGHT
+const light = new THREE.AmbientLight('#8787ff', 1);//#fc4c39 #8787ff
+scene.add(light);
+const pointLight = new THREE.PointLight('#fc4c39', 1);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
 
+//---------------------------------------------------------------------------------
 //bloom
 const renderScene = new RenderPass( scene, camera );
 const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
@@ -149,9 +147,6 @@ let composer = new EffectComposer( renderer );
 //composer.renderToScreen = false;
 composer.addPass( renderScene );
 composer.addPass( bloomPass );
-
-
-
 
 
 //------------------------------------events----------------------------------
@@ -229,16 +224,12 @@ const render = () => {
     cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
 
-    //renderer.clear()
-    //camera.layers.set(1)
+   
     //render bloom past 
-    composer.render();
+    //composer.render();
 
-    
-    //renderer.clearDepth()
-    //camera.layers.set(0)
     //render
-    //renderer.render(scene, camera);
+    renderer.render(scene, camera);
     window.requestAnimationFrame(render)
   stats.end();
 }
